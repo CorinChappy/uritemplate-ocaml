9944ce2d0bc3eea0cb869d03b17d8cce1c17d944
Corin Chaplin <git@corinchaplin.co.uk>
2019-03-14 18:33:46 +0000

    Add odoc comment to template_uri

--- src/uritemplate.mli
+++ src/uritemplate.mli
@@ -1,1 +1,3 @@
 val template_uri: template:string -> variables:(string * string) list -> string
+(** Templates the given string using the provided variables.
+    {e Currently only compliant to level 3.} See {{:https://tools.ietf.org/html/rfc6570}RFC6570} *)

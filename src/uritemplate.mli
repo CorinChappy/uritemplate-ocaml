8360d0f2b7b6e240a389e7405b196eab8b9fac0b
Corin Chaplin <git@corinchaplin.co.uk>
2019-03-28 22:28:18 +0000

    Update odoc comment for template_uri

--- src/uritemplate.mli
+++ src/uritemplate.mli
@@ -7,7 +7,7 @@ type variable = [
 
 val template_uri: template:string -> variables:(string * variable) list -> string
 (** Templates the given string using the provided variables.
-    {e Currently only compliant to level 3.} See {{:https://tools.ietf.org/html/rfc6570}RFC6570} *)
+    {e Currently compliant to level 4.} See {{:https://tools.ietf.org/html/rfc6570}RFC6570} *)
 
 val template_uri_with_strings: template:string -> variables:(string * string) list -> string
 (** A shorthand for template_uri when all variables are single strings *)
